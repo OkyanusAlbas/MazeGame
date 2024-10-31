39f7b6e426c9cc514b4e6b00ad0a948fadcd1266
OkyanusAlbas <albaokya@icloud.com>
2024-10-31 15:25:32 +0000

    first commit

---
+++ MazeGame.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Include the iostream library for input and output
+using namespace std;
+
+int main() 
+{
+    cout << "Welcome to my game!" << endl; // Output a welcome message
+    return 0; // Return 0 to indicate that the program ended successfully
+}
